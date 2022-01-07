ed63c46b35e369824be84bdb914a296a14b97889
bovage <fayemibolu2@gmail.com>
2022-01-06 23:21:43 -1200

    create Message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='message', on_delete=models.CASCADE)
+    message_text = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message_text
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:10]
