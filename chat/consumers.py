649cc9182eafd3cb2d12eaaa0fca9101dc2b1b3d
bovage <fayemibolu2@gmail.com>
2022-01-11 17:53:42 -1200

    change consumer to sync

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,45 +1,63 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 from chat.models import Message
 import json
 
-class ChatRoomConsumer(AsyncWebsocketConsumer):
+class ChatRoomConsumer(WebsocketConsumer):
 
     def fetch_messages(self, data):
-        print('fetching...')
+        messages = Message.last_10_messages()
+        content = {
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_chat_message
 
 
     def new_message(self, data):
         print('get new messages...')
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.message_text,
+            'timestamp': str(message.timestamp)
+        }
 
     commands = {
         'fetch_messages': fetch_messages,
         'new_message': new_message
     }
 
-    async def connect(self):
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
-        await  self.channel_layer.group_add (
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard) (
             self.room_group_name,
             self.room_name
         )
 
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json['username']
         self.commands[text_data_json['command']](self, text_data)
 
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chatroom_message',
@@ -48,11 +66,11 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    async def chatroom_message(self, event):
+    def chatroom_message(self, event):
         message = event['message']
         username = event['username']
 
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'username': username,
         }))
